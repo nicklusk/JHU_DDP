aa48a99022ae5edf423919d88b348fd7076b29cc
mbh038 <mike038@btinternet.com>
2015-08-08 06:47:26 +0100

    Quiz 1

---
+++ Quiz1/quiz1.R
@@ -0,0 +1,10 @@
+library(manipulate)
+myPlot <- function(s) {
+        plot(cars$dist - mean(cars$dist), cars$speed - mean(cars$speed))
+        abline(0, s)
+}
+
+ manipulate(myPlot(s), s = slider(0, 2, step = 0.1)) #yes!
+# manipulate(myPlot(s), slider = x(0, 2, step = 0.1)) no!
+# manipulate(myPlot(s), x.s = slider(0, 2, step = 0.1)) no!
+#  manipulate(myPlot, s = slider(0, 2, step = 0.1)) no!
